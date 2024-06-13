e2ae70fa0804d83d65bf4d0c54a5cd3134578c02
Nazg0r <gorchinshiy83@gmail.com>
2024-06-13 13:28:51 +0300

    Add Users Async Controllers

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _dbContext;
+
+		public UsersController(DataContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+		{
+			IEnumerable<User> users = await _dbContext.Users.ToListAsync();
+
+			return Ok(users);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<User>> GetUser(int id)
+		{
+			User user = await _dbContext.Users.FindAsync(id);
+
+			return Ok(user);
+		}
+
+	}
+}
+
