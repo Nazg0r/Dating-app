40c3486f65356ab1f046dc38a9bec15a42ab9a4d
Nazg0r <gorchinshiy83@gmail.com>
2024-07-01 13:28:36 +0300

    Add login method to account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace API.Controllers
 {
@@ -37,9 +38,35 @@ namespace API.Controllers
 			return Ok(user);
 		}
 
+		[HttpPost("login")]
+		public async Task<ActionResult<User>> Login(LoginDTO loginDTO)
+		{
+			if (!await UserExist(loginDTO.name)) return Unauthorized("Invalid username");
+
+			User user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Name == loginDTO.name);
+
+			if (!await CheckPassword(user, loginDTO.password)) return Unauthorized("Invalid password");
+
+			return Ok(user);
+		}
+
 		private async Task<bool> UserExist(string name)
 		{
 			return await _dbContext.Users.AnyAsync( u => u.Name.ToLower() == name.ToLower());
+		}
+
+		private async Task<bool> CheckPassword(User user, string password)
+		{
+			using HMAC hmac = new HMACSHA512(user.Salt);
+
+			byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            for (int i = 0; i < user.Password.Length; i++)
+            {
+                if (user.Password[i] != computedHash[i]) return false;
+            }
+
+            return true;
 		}
 	}
 }
